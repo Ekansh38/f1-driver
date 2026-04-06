27d7ad388d74e710535e9d816ba165e5ffb963b8
Byte Colony <goenka.ekansh@gmail.com>
2026-04-06 15:50:55 +0800

    fix: drop Intel runner, arm64-only release build

--- Formula/f1-driver.rb
+++ Formula/f1-driver.rb
@@ -8,10 +8,6 @@ class F1Driver < Formula
       url "https://github.com/Ekansh38/f1-driver/releases/download/v1.0.0/f1-driver-macos-arm64.tar.gz"
       sha256 "PLACEHOLDER_ARM64"
     end
-    on_intel do
-      url "https://github.com/Ekansh38/f1-driver/releases/download/v1.0.0/f1-driver-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_X86"
-    end
   end
 
   def install
